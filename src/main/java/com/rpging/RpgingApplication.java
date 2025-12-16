804ff92304b0db746c89e41256252070aeddfbe7
abraaorochapb <abraaorochapb@gmail.com>
2025-12-16 08:10:56 -0300

    project setup

---
+++ src/main/java/com/rpging/RpgingApplication.java
@@ -0,0 +1,13 @@
+package com.rpging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgingApplication.class, args);
+	}
+
+}
